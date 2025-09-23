7d2104de8bdaedfd0d91ac808acb3510740702e1
Phattharachai Leeyutthanon <phattharachai2547@gmail.com>
2025-09-23 14:05:29 +0700

    Modifile position of uploadbox

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,7 +1,7 @@
 import Uploadbox from "../components/uploadbox";
 const Landing = () => {
   return (
-    <div className="flex w-full h-screen mt-10 ml-10">
+    <div className="flex w-full h-screen mt-10  justify-center ">
       <Uploadbox />
     </div>
   );
