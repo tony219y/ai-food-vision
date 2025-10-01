1f08cc6e39223b631df3ed3b88d0a27a4e87f349
tony219y <aekanan2004@gmail.com>
2025-10-01 21:50:13 +0700

    fix: add origin allow CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="http://localhost:5173",
+    allow_origins=["http://localhost:5173","https://ai-food-vision.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
